68c25fbe44a34514b25e2f8747e2aabb4e805bd5
José Alonso López Cárdenas <jose.lopez.car@tecsup.edu.pe>
2026-06-18 11:45:11 -0500

    Creación de repositorios

--- app/src/main/java/com/example/cosmicmuseum/data/repository/NasaRepository.kt
+++ app/src/main/java/com/example/cosmicmuseum/data/repository/NasaRepository.kt
@@ -1,2 +1,11 @@
 package com.example.cosmicmuseum.data.repository
 
+import com.example.cosmicmuseum.data.remote.NasaDto
+import com.example.cosmicmuseum.data.remote.RetrofitInstance
+
+class NasaRepository {
+
+    suspend fun getApod(): NasaDto {
+        return RetrofitInstance.api.getApod("DEMO_KEY")
+    }
+}
--- app/src/main/java/com/example/cosmicmuseum/data/repository/TicketRepository.kt
+++ app/src/main/java/com/example/cosmicmuseum/data/repository/TicketRepository.kt
@@ -1,2 +1,30 @@
 package com.example.cosmicmuseum.data.repository
 
+import com.example.cosmicmuseum.data.local.TicketDao
+import com.example.cosmicmuseum.data.local.TicketEntity
+import kotlinx.coroutines.flow.Flow
+
+class TicketRepository(
+    private val ticketDao: TicketDao
+) {
+
+    fun getAllTickets(): Flow<List<TicketEntity>> {
+        return ticketDao.getAllTickets()
+    }
+
+    suspend fun getTicketById(id: Int): TicketEntity? {
+        return ticketDao.getTicketById(id)
+    }
+
+    suspend fun insertTicket(ticket: TicketEntity) {
+        ticketDao.insert(ticket)
+    }
+
+    suspend fun updateTicket(ticket: TicketEntity) {
+        ticketDao.update(ticket)
+    }
+
+    suspend fun deleteTicket(ticket: TicketEntity) {
+        ticketDao.delete(ticket)
+    }
+}
